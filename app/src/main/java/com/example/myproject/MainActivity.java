d25cad8e35156328ab42f6ce025b7779f900dd71
Broxigar <email>
2021-11-21 22:49:22 +0100

    squash! readme

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -1,16 +1,52 @@
 package com.example.myproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Storage[] kanjiList = new Storage [5];
+    TextView kanjiInquiry;
+    EditText answerInput;
+    AppCompatButton NextButton;
+    int currentInquiry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        kanjiList[0] = new Storage("一", "one", "いち");
+        kanjiList[1] = new Storage("二", "two", "に");
+        kanjiList[2] = new Storage("三", "three", "さん");
+        kanjiList[3] = new Storage("四", "four", "し");
+        kanjiList[4] = new Storage("五", "five", "ご");
+
+        kanjiInquiry = findViewById(R.id.kanjiInquiry);
+        answerInput = findViewById(R.id.answerInput);
+        NextButton = findViewById(R.id.NextButton);
+        currentInquiry=0;
+        kanjiInquiry.setText(kanjiList[currentInquiry].kanji);
+
+
+
     }
 
+    public void OnNextButtonClick (View view){
+        if(answerInput.getText().toString().toLowerCase().equals(kanjiList[currentInquiry].meaning)){
+            if(currentInquiry>3) currentInquiry=0;
+            else currentInquiry++;
+            kanjiInquiry.setText(kanjiList[currentInquiry].kanji);
+            answerInput.setText("");
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "fuck", Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/myproject/Storage.java
@@ -0,0 +1,14 @@
+package com.example.myproject;
+
+public class Storage {
+    public String kanji;
+    public String meaning;
+    public String reading;
+
+    public Storage(String s1, String s2, String s3) {
+        this.kanji = s1;
+        this.meaning = s2;
+        this.reading = s3;
+    }
+
+}
